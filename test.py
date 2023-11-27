eec131a83f617bc13ee81b4b69df21aab8ecb490
Diego Morales <diegomorales1359@gmail.com>
2023-11-26 21:59:43 -0600

    test

--- test.py
+++ test.py
@@ -7,11 +7,13 @@ from utils.utils import show_masks
 from build_sam import sam_model_registry
 
 def testChido():
-    print('hola')
-    # sam = sam_model_registry['vit_b'](checkpoint='sam_vit_h_4b8939.pth').cpu()
+
+    sam = sam_model_registry['vit_b'](checkpoint='sam_vit_b_01ec64.pth').cpu()
+    print(os.listdir(os.getcwd()))
+
     # auto_to_mask = SamMaskGenerator(sam, stability_score_thresh=0.8)
 
     # # image upload
     # img = np.array(Image.open("figure/paris2.jpg"))
     # masks = auto_to_mask.generate(img)
-    return os.listdir(os.getcwd())
+    return 'checar'
